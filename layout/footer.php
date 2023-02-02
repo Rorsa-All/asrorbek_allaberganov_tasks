b8c59f1d64da1dae7299f3577bdeb515cf63d355
Rorsa-All <my.journey.h@gmail.com>
2023-02-02 20:00:08 +0300

    footer

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
 </div>
 </div>
 <div class="row">
-  <footer style="background-color: rgb(65, 7, 109); color: white;">
+  <footer  color: white;">
 
     <!-- Copyright -->
     <div class="footer-copyright text-center py-3">© 2020 Copyright
