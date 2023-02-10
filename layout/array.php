d37c89bd108a3886d275570a0787aa6aaf72f2ce
Rorsa-All <my.journey.h@gmail.com>
2023-02-10 14:28:45 +0300

    add

--- layout/array.php
+++ layout/array.php
@@ -54,12 +54,45 @@ $color = array("red"=>"#FF0000", "green"=>"#2BFF00", "black"=>"#000000", "white"
 foreach($color as $colorn=>$val) {
     echo "$colorn=" . $val . "<br>";
 }
-$a = array(
-    "one" => 1,
-    "two" => 2,
-    "three" => 3,
-    "seventeen" => 17
+?>
+
+<h4>6.average temperature</h4>
+<?php
+ $temp=array(78, 60, 62, 68, 71, 68, 73,
+ 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 
+ 74, 62, 62, 65, 64, 68, 73, 75, 79, 73
 );
+
+echo "<hr><h2> Calculation average temperature: </h2>";
+$month_temp = "78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 81, 76, 73,
+68, 72, 73, 75, 65, 74, 63, 67, 65, 64, 68, 73, 75, 79, 73";
+// what is explode and what does the below code do? : 
+$temp_array = explode(',', $month_temp);
+$tot_temp = 0;
+// What is count?
+$temp_array_length = count($temp_array);
+foreach($temp_array as $temp)
+{
+ $tot_temp += $temp;
+}
+ $avg_high_temp = $tot_temp/$temp_array_length;
+ echo "Average Temperature is : ".$avg_high_temp."
+"; 
+// what does sort do?
+sort($temp_array);
+echo "<br> List of five lowest temperatures :";
+for ($i=0; $i< 5; $i++)
+{ 
+echo $temp_array[$i].", ";
+}
+echo "<br>List of five highest temperatures :";
+// explain the following loop
+for ($i=($temp_array_length-5); $i< ($temp_array_length); $i++)
+{
+echo $temp_array[$i].", ";
+}
+?>
+
 
 ?>
 
